32369e59a5351807fdbcc366916e7a88d1c348cc
AB <ryancoelho.vale@gmail.com>
2022-12-25 16:45:02 -0300

    e

---
+++ scripts/serpulo.js
@@ -0,0 +1,7 @@
+Planets.serpulo.allowSectorInvasion = false;
+ruleSetter = r => {
+    r.waveTeam = Team.crux;
+    r.placeRangeCheck = false;
+    r.showSpawns = true;
+};
+atmosphereColor = Color.valueOf("#1f10ff");
