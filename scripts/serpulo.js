6b05ff6824d92707166e22bf4913b2935234fdc6
AB <ryancoelho.vale@gmail.com>
2023-09-01 20:53:29 -0300

    hfjzxvh

--- scripts/serpulo.js
+++ scripts/serpulo.js
@@ -1,4 +1,4 @@
-Planets.serpulo.allowSectorInvasion = false;
+Planets.serpulo.allowSectorInvasion = true;
 Planets.serpulo.atmosphereColor = Color.valueOf("ff0000");
 Planets.serpulo.orbitRadius = 30;
 Planets.serpulo.ruleSetter = r => {
