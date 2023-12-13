6cbcfea8d1178061f898054f022dbf7df31ed076
AB <70777952+RayanZ22@users.noreply.github.com>
2023-12-13 11:09:41 -0300

    e

--- scripts/serpulo.js
+++ scripts/serpulo.js
@@ -1,8 +1,10 @@
 Planets.serpulo.allowSectorInvasion = true;
 Planets.serpulo.atmosphereColor = Color.valueOf("#ffffff");
-Planets.serpulo.orbitRadius = 15;
+Planets.serpulo.orbitRadius = 7;
 Planets.serpulo.ruleSetter = r => {
     r.waveTeam = Team.crux;
     r.placeRangeCheck = false;
     r.showSpawns = true;
+    r.allowWaveSimulation = true;
+    r.deconstructRefundMultiplier = 1
 };
