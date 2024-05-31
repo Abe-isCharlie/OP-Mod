efacf493ff3278b8968941f330a0cf4343290f03
Abe-isCharlie <ryancoelho.vale@gmail.com>
2024-05-31 16:29:42 -0300

    update teleporter.js

--- scripts/teleporter.js
+++ scripts/teleporter.js
@@ -1,31 +1,12 @@
-// Define o massBullet para o teleporter
-const massBullet = extend(MassDriverBolt, {
-  draw(b) {
-    var w = 0.001;
-    var h = 0.001;
-
-    Draw.color(Pal.bulletYellowBack);
-    Draw.rect("shell-back", b.x, b.y, w, h, b.rotation() + 90);
-
-    Draw.color(Pal.bulletYellow);
-    Draw.rect("shell", b.x, b.y, w, h, b.rotation() + 90);
-
-    Draw.reset();
-  },
-  despawnEffect: Fx.none,
-  hitEffect: Fx.none,
-  collides: false,
-});
-
 // Define o teleporter
 const teleporter = extend(BufferedItemBridge, "teleporter", {
   // Defina os requisitos para construir o teleporter
-  requirements(Category.distribution, ItemStack.with(
+  requirements: ItemStack.with(
     Items.phaseFabric, 10,
     Items.silicon, 15,
     Items.lead, 20,
     Items.graphite, 20
-  )),
+  ),
 
   range: 20, // Define um alcance maior
   arrowPeriod: 0.5,
@@ -35,7 +16,7 @@ const teleporter = extend(BufferedItemBridge, "teleporter", {
   envEnabled: Env.space, // Habilita o teleporter em ambientes espaciais
 
   // Consome energia para funcionar
-  consumePower(1.0),
+  consumePower: 1.0,
 
   buildType: () => extend(BufferedItemBridge.BufferedItemBridgeBuild, teleporter, {
     update() {
@@ -52,4 +33,13 @@ const teleporter = extend(BufferedItemBridge, "teleporter", {
       }
     }
   })
+});
+
+// Carregue o teleporter na categoria de distribuição
+teleporter.category = Category.distribution;
+
+// Certifique-se de que o código do mod está sendo carregado corretamente
+Events.on(ClientLoadEvent, () => {
+  Vars.content.units().each(e => e.health = e.maxHealth);
+  Log.info("Teleporter mod loaded.");
 });
