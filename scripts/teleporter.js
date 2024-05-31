3c95e7f5f3139236c801bba4121a7fc87b307d6f
Abe-isCharlie <ryancoelho.vale@gmail.com>
2024-05-31 17:05:27 -0300

    update teleporter.js

--- scripts/teleporter.js
+++ scripts/teleporter.js
@@ -55,7 +55,3 @@ const teleporter = extend(BufferedItemBridge, "teleporter", {
 });
 
 // Certifique-se de que o código do mod está sendo carregado corretamente
-Events.on(ClientLoadEvent, () => {
-  Vars.content.units().each(e => e.health = e.maxHealth);
-  Log.info("Teleporter mod loaded.");
-});
