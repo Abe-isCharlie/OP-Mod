21dc32cc4c47e88bea2ef0aa9a8128cb17393388
Abe-isCharlie <ryancoelho.vale@gmail.com>
2024-06-05 23:16:03 -0300

    update

--- scripts/serpulo.js
+++ scripts/serpulo.js
@@ -1,7 +1,7 @@
 Planets.serpulo.allowSectorInvasion = true;
 Planets.serpulo.atmosphereColor = Color.valueOf("#FF0000");
-Planets.serpulo.orbitRadius = 13;
+Planets.serpulo.orbitRadius = 20;
 Planets.serpulo.ruleSetter = (r) => {
   r.showSpawns = true;
-  r.deconstructRefundMultiplier = 1.75;
+  r.deconstructRefundMultiplier = 1.99;
 };
