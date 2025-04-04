308d00eb1b8b51192cb0a9bd0e7979c33e444e93
AB <70777952+Abe-isCharlie@users.noreply.github.com>
2025-04-04 03:25:50 -0300

    Update serpulo.js
    Remove to force serpulo invasions

--- scripts/serpulo.js
+++ scripts/serpulo.js
@@ -1,4 +1,4 @@
-Planets.serpulo.allowSectorInvasion = true;
+//Planets.serpulo.allowSectorInvasion = true;  Removed by convenience.
 Planets.serpulo.atmosphereColor = Color.valueOf("#ffee22");
 Planets.serpulo.orbitRadius = 70;
 Planets.serpulo.ruleSetter = (r) => {
