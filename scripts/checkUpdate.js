428181c93878dad35ef9ebe2187a8f66837bf79b
AB <ryancoelho.vale@gmail.com>
2025-10-23 11:16:22 -0300

    asdads

--- scripts/checkUpdate.js
+++ scripts/checkUpdate.js
@@ -3,43 +3,38 @@ var currentVersion = "1.3.0";
 var versionURL =
   "https://raw.githubusercontent.com/Abe-isCharlie/OP-Mod/main/mod.json";
 
-print("checkUpdate.js loaded!");
-
 Events.on(EventType.ClientLoadEvent, function () {
-  print("ClientLoadEvent triggered!");
-  print("Checking for updates at: " + versionURL);
+  // Adiciona timestamp pra evitar cache
+  var urlWithCacheBuster = versionURL + "?t=" + Packages.arc.util.Time.millis();
 
   Http.get(
-    versionURL,
+    urlWithCacheBuster,
     function (res) {
-      print("HTTP Response received!");
-      var responseText = res.getResultAsString();
-      print("Response: " + responseText);
-
       try {
-        var data = Packages.arc.util.serialization.Jval.read(responseText);
-        var latestVersion = data.getString("version"); // SEM o segundo parâmetro!
-
-        print("Current version: " + currentVersion);
-        print("Latest version: " + latestVersion);
+        var data = Packages.arc.util.serialization.Jval.read(
+          res.getResultAsString()
+        );
+        var latestVersion = data.getString("version");
 
         if (latestVersion !== currentVersion) {
-          print("Versions differ! Showing toast...");
-          Vars.ui.showInfoToast(
-            "[orange]Update available![]\nGo to the Mods tab and click 'Reinstall' on OP Things.",
-            8
-          );
-        } else {
-          print("Versions match, no update needed.");
+          // Usa Core.app.post() pra garantir que a UI tá pronta
+          Core.app.post(function () {
+            Vars.ui.showInfoToast(
+              "[orange]Update available![]\nCurrent: " +
+                currentVersion +
+                ", Latest: " +
+                latestVersion +
+                "\nGo to Mods and click 'Reinstall' on OP Things.",
+              10
+            );
+          });
         }
       } catch (e) {
-        print("Error parsing JSON: " + e);
+        print("Error checking update: " + e);
       }
     },
     function (err) {
-      print("HTTP request failed: " + err);
+      print("Error checking update: " + err);
     }
   );
 });
-
-print("Event listener registered!");
