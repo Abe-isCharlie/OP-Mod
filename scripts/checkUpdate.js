a821273200c24287379a526b0a452d2f75d79c63
AB <ryancoelho.vale@gmail.com>
2025-10-23 10:51:23 -0300

    aaaaaaaaaaaaa

--- scripts/checkUpdate.js
+++ scripts/checkUpdate.js
@@ -1,26 +1,33 @@
+const Events = require("mindustry/game/Events");
+const ClientLoadEvent = Packages.mindustry.game.EventType.ClientLoadEvent;
+
 const currentVersion = "1.2.6";
 const repoURL = "https://github.com/Abe-isCharlie/OP-Mod";
 const versionURL =
   "https://raw.githubusercontent.com/Abe-isCharlie/OP-Mod/main/mod.json";
 
-Event.on(CLientLoadEvent, () => {
+Events.on(ClientLoadEvent, () => {
   Http.get(
     versionURL,
     (res) => {
       try {
-        const data = JSON.parse(res.getResultAsString());
-        if (data.version && data.version !== currentVersion) {
+        const data = Packages.arc.util.serialization.Jval.read(
+          res.getResultAsString()
+        );
+        const latestVersion = data.getString("version", currentVersion);
+
+        if (latestVersion !== currentVersion) {
           Vars.ui.showInfoToast(
-            `[orange]Update available![]\nGo to the Mods tab and click 'Reinstall' on OP Things.`,
+            `[orange]Atualização disponível![]\nVá até a aba Mods e clique em 'Reinstalar' no OP Things.`,
             8
           );
         }
       } catch (e) {
-        print("Error checking update: " + e);
+        print("Erro ao verificar atualização: " + e);
       }
     },
     (err) => {
-      print("Error checking update: " + err);
+      print("Erro ao verificar atualização: " + err);
     }
   );
 });
