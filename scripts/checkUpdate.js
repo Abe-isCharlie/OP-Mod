ef069103f4bbba9bd1028eefd4fb76deade3db34
AB <ryancoelho.vale@gmail.com>
2025-10-23 11:05:11 -0300

    aaaaaaaaaaaaaaaaaa

--- scripts/checkUpdate.js
+++ scripts/checkUpdate.js
@@ -1,23 +1,39 @@
 // checkUpdate.js
-var currentVersion = "1.3.0";
+var currentVersion = "1.2.6";
 var versionURL = "https://raw.githubusercontent.com/Abe-isCharlie/OP-Mod/main/mod.json";
 
+print("checkUpdate.js loaded!");
+
 Events.on(EventType.ClientLoadEvent, function() {
+  print("ClientLoadEvent triggered!");
+  print("Checking for updates at: " + versionURL);
+  
   Http.get(versionURL, function(res) {
+    print("HTTP Response received!");
+    print("Response: " + res.getResultAsString());
+    
     try {
       var data = Packages.arc.util.serialization.Jval.read(res.getResultAsString());
       var latestVersion = data.getString("version", currentVersion);
       
+      print("Current version: " + currentVersion);
+      print("Latest version: " + latestVersion);
+      
       if (latestVersion !== currentVersion) {
+        print("Versions differ! Showing toast...");
         Vars.ui.showInfoToast(
           "[orange]Update available![]\nGo to the Mods tab and click 'Reinstall' on OP Things.",
           8
         );
+      } else {
+        print("Versions match, no update needed.");
       }
     } catch (e) {
-      print("Error checking update: " + e);
+      print("Error parsing JSON: " + e);
     }
   }, function(err) {
-    print("Error checking update: " + err);
+    print("HTTP request failed: " + err);
   });
 });
+
+print("Event listener registered!");
