bc6b37538b0a652aa989971da4dea8ccdb60edcd
AB <ryancoelho.vale@gmail.com>
2025-10-23 11:18:09 -0300

    debug

--- scripts/checkUpdate.js
+++ scripts/checkUpdate.js
@@ -7,16 +7,24 @@ Events.on(EventType.ClientLoadEvent, function () {
   // Adiciona timestamp pra evitar cache
   var urlWithCacheBuster = versionURL + "?t=" + Packages.arc.util.Time.millis();
 
+  print("Checking for updates at: " + urlWithCacheBuster);
+
   Http.get(
     urlWithCacheBuster,
     function (res) {
+      print("HTTP Response received!");
+      var responseText = res.getResultAsString();
+      print("Response: " + responseText);
+
       try {
-        var data = Packages.arc.util.serialization.Jval.read(
-          res.getResultAsString()
-        );
+        var data = Packages.arc.util.serialization.Jval.read(responseText);
         var latestVersion = data.getString("version");
 
+        print("Current version: " + currentVersion);
+        print("Latest version: " + latestVersion);
+
         if (latestVersion !== currentVersion) {
+          print("Versions differ! Showing toast...");
           // Usa Core.app.post() pra garantir que a UI tá pronta
           Core.app.post(function () {
             Vars.ui.showInfoToast(
@@ -28,13 +36,15 @@ Events.on(EventType.ClientLoadEvent, function () {
               10
             );
           });
+        } else {
+          print("Versions match, no update needed.");
         }
       } catch (e) {
-        print("Error checking update: " + e);
+        print("Error parsing JSON: " + e);
       }
     },
     function (err) {
-      print("Error checking update: " + err);
+      print("HTTP request failed: " + err);
     }
   );
 });
