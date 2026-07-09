b94b42ded8b953cf472c8e2486e3dd6f1aa53196
AB <ryancoelho.vale@gmail.com>
2026-07-09 19:33:04 -0300

    jez

--- scripts/serpulo.js
+++ scripts/serpulo.js
@@ -2,6 +2,6 @@
 Planets.serpulo.atmosphereColor = Color.valueOf("#ffee22");
 Planets.serpulo.orbitRadius = 70;
 Planets.serpulo.ruleSetter = (r) => {
-  r.showSpawns = true;
+  r.hideSpawns = false;
   r.deconstructRefundMultiplier = 1.99;
 };
